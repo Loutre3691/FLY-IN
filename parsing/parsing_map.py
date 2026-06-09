768c4cbfe890e87dedda606fbbda4644581647ef
Flora De Checchi <flora.dechecchi@gmail.com>
2026-06-09 15:42:30 +0200

    creation de la methode parse_line dans la class waypoint qui permet de parser les doublons de nom de station et les doublons de coordonnes en premier lieu

--- parsing/parsing_map.py
+++ parsing/parsing_map.py
@@ -3,15 +3,17 @@ from pydantic import Field, BaseModel, ValidationError
 from abc import ABC
 
 
+name_and_coordinate = {}
+
 class ConfigParsing():
     """
    Lit le fichier de configuration du drone et initialise le processus d'analyse.
 
-Arguments:
-map_read (io.TextIOWrapper): Le fichier de configuration ouvert.
+    Arguments:
+    map_read (io.TextIOWrapper): Le fichier de configuration ouvert.
 
-Attributs:
-map_read (list[str]): Toutes les lignes brutes lues dans le fichier.
+    Attributs:
+    map_read (list[str]): Toutes les lignes brutes lues dans le fichier.
     """
     def __init__(self, map) -> None:
         self.map_read = map.readlines()
@@ -24,17 +26,17 @@ map_read (list[str]): Toutes les lignes brutes lues dans le fichier.
         
     def dispatch_parsing(self, map: list[str]):
         """
-Analyse les lignes de configuration nettoyées et les convertit en dictionnaire.
-Sépare chaque ligne au niveau du premier «:» pour extraire la clé et la valeur.
-Les clés répétées (par exemple, «hub», «connection») sont regroupées dans une liste.
-map (liste[str]): Lignes nettoyées du fichier de configuration.
+        Analyse les lignes de configuration nettoyées et les convertit en dictionnaire.
+        Sépare chaque ligne au niveau du premier «:» pour extraire la clé et la valeur.
+        Les clés répétées (par exemple, «hub», «connection») sont regroupées dans une liste.
+        map (liste[str]): Lignes nettoyées du fichier de configuration.
 
-Résultat final après dispatch_parsing:
-{
-"nb_drones": ["2"],
-"hub": ["waypoint1 1 0 [color=blue]", "waypoint2 2 0 [color=blue]"],
-"connection": ["waypoint1-waypoint2"]
-}
+        Résultat final après dispatch_parsing:
+        {
+        "nb_drones": ["2"],
+        "hub": ["waypoint1 1 0 [color=blue]", "waypoint2 2 0 [color=blue]"],
+        "connection": ["waypoint1-waypoint2"]
+        }
     """
         new_dict: dict = {}
         order_key = {
@@ -69,24 +71,28 @@ Résultat final après dispatch_parsing:
             print("Error, first line muste be 'nb_drones' end min 1 drone in value and max 999 999")
         
         try:
-            value_start_hub = new_dict.get("start_hub", [None])[0]
-            
-            ConfigStart.name_start(value_start_hub)
+            value_start_hub = new_dict.get("start_hub", [None])
+            ConfigStartHub.parse_line(value_start_hub)
+            value_hub = new_dict.get("hub", [None])
+            ConfigHub.parse_line(value_hub)
+            value_end_hub = new_dict.get("end_hub", [None])
+            ConfigEndHub.parse_line(value_end_hub)
+        
 
         except ValidationError:
-            print("Error start")
+            print("Error,  start_hub")
         
 
 
 class ConfigMap():
     """
-Nettoie les lignes de texte brut en supprimant les espaces, les lignes vides et les commentaires.
+    Nettoie les lignes de texte brut en supprimant les espaces, les lignes vides et les commentaires.
 
-Arguments:
-maplines (liste [str]): Lignes brutes du fichier de configuration.
+    Arguments:
+    maplines (liste [str]): Lignes brutes du fichier de configuration.
 
-Attributs:
-cleaned_line (liste [str]): Lignes filtrées ne contenant que les données.
+    Attributs:
+    cleaned_line (liste [str]): Lignes filtrées ne contenant que les données.
     """
     @staticmethod
     def clean_file_txt(maplines) -> list:
@@ -101,29 +107,61 @@ cleaned_line (liste [str]): Lignes filtrées ne contenant que les données.
             
 
 class ConfigDrone(BaseModel):
-    nb_drones: int = Field(ge=1, le=999999)
+    '''
+    Petite classe Basemodel, permet juste de gerer le nombre de drone pour le moment, a voir pour l integre
+    plus tard dans une autre classe
+    '''
+    nb_drones: int = Field(ge=1, le=200)
 
-# class ConfigGlobal(ABC):
-#     def __init__(self, zone_1, zone_2) -> None:
-#         pass
 
-# class Waypoint(ABC):
-#     def __init__(self, name, x, y, zone, color, max):
-#         pass
+class Waypoint(ABC):
+    '''
+    Waypoint qui est une classe abstraite permet  de parser chaque ligne selon sa cle (start_hub,
+    hub, end_hub) ps: connection est gerer dans une autre classe car n'a pas les memes arguments
+    '''
+    def __init__(self, name, x, y, zone, color, max_drones):
+        self.name = name
+        self.x = x
+        self.y = y
+        self. zone = zone
+        self.color = color
+        self.max = max_drones
+    
 
-class ConfigStart():
-    def __init__(self) -> None:
-        pass
-        # super().__init__(name, x, y, zone, color, max)
+    def parse_line(value) -> None:
+        '''
+        parse_line permet de split chaque ligne, on separe en premier lieu le nom et x, y des option entre crochet,
+        creation d'un dictionnaire avec name de la station et un tupple de coordonne x, y puis gestion des erreurs
+        grace au doublon des cles du dictionnaire (nom station) et ensuite avec un set pour gerer les erreurs
+        des doublons de coordonnees
+        '''
+        for first_split in value:
+            part = first_split.split("[")
+            main_part = part[0].split(" ")
+            name_station, x, y = main_part[0], main_part[1], main_part[2]
+            option_part = part[1].split("[")
 
-    def name_start(party) -> None:
-        part = party.split("[", 1)
-        part_principal = part[0].split(" ", 2)
-        part_option = part[1].split(" ")
-        print(part_principal)
-        print (part_option)
+            if name_station in name_and_coordinate:
+                raise ValueError(f"Duplicate not allowed: The station '{name_station}' already exists in the dictionary.")
+            else:
+                name_and_coordinate[name_station]= x, y
         
+        single_coordinate = list(name_and_coordinate.values())
+        set_coordinate = set(single_coordinate)
+
+        if len(single_coordinate) != len(set_coordinate):
+            raise ValueError((f"Duplicate not allowed: coordinate exists in duplicate"))
+
         
-        
-            
-       
+
+
+
+class ConfigStartHub(Waypoint):
+    pass
+
+
+class ConfigHub(Waypoint):
+    pass
+    
+class ConfigEndHub(Waypoint):
+    pass
