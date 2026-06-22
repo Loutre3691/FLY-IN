b1849013603f8a748d787d3ed61520966f01cac1
Flora De Checchi <flora.dechecchi@gmail.com>
2026-06-22 20:40:32 +0200

    simulator: il faut maintenant incorporer dijkstra dans ligne 29 pour chaque station et ensuite selon le chemin qui ressort prendre le premier voisin qui ressort et le faire avancer au fur et a mesure enfin voila

--- pathfinder/simulator.py
+++ pathfinder/simulator.py
@@ -6,30 +6,45 @@ class SimulatorDrones():
     # dijkstra renvoi le chemin pour un drone le best
     def __init__(self, nb_drones, stations_data, neighbor_station, connections_data) -> None:
         self.nb_drones = nb_drones
-        self.current_drones = {}
+        self.current_drones = {} # max drone par station et quantite de drone par station
         self.stations_data = stations_data
-
-
-        self.current_drones['start']['current'] = nb_drones
-        self.current_drones['goal']['current'] = float('inf')
-
 
         for station in stations_data:
             if stations_data[station].get('max_drones'):
                 self.current_drones[station] = {"max": int(stations_data[station]['max_drones']), "current": 0}
             else:
                 self.current_drones[station] = {"max": float('inf'), "current": 0}
-
-
-        while nb_drones:
+        print(self.current_drones)
+        print()
+        print(neighbor_station)
+        print()
+        self.current_drones['start']['current'] = nb_drones
+        print(self.current_drones)
+        
+        # parcourir les stations une a une, et recupereer son voisin voir si chaque voisin a la possibilite 
+        # d accueillir un drone grace a la diff entre current et max, si de la place le current de la station de base
+        # se vide et lautre se rempli selon le calcul dijkstra
+        while nb_drones > 0:
             for station in self.current_drones:
-                for neighbor in neighbor_station:
-                    if station == neighbor:
-                        for neighbor_station[neighbor] in neighbor:
-                            if self.current_drones[station]['current'] <= self.current_drones[station]['max']:
-                                self.current_drones[station]['current'] += 1
-                        
-            nb_drones -= 1
-                            
+                
+                check_neighbors = neighbor_station[station]
+                for neighbor in check_neighbors:
+                    if neighbor in self.current_drones:
+                  
+        
+            
+                    
 
+                # if neighbor_station[station]['current'] <= neighbor_station[station]['max']:
+                #     neighbor_station[station]['current'] += 1
+                        
+                # print() 
+                # print(self.current_drones)
+                # print("-----------------------------------------")
+            # nb_drones -= 1
+                
+                            
+        
+        
+                            
 
